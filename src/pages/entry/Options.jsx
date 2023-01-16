3a7dfe8e6e85e9055b28a2c87ebfbaca0fa6d320
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2023-01-16 08:37:42 -0300

    add: topping options, tests: add topping and error

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,3 +1,33 @@
-export default function Options({ optionsType }) {
-  return <div></div>;
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import { useEffect, useState } from "react";
+import ScoopOption from "./ScoopOptions";
+import ToppingOptions from "./ToppingOptions";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        // TODO: handle error response
+      });
+  }, [optionType]);
+
+  // TODO: replace "null" with ToppingOption
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOptions;
+
+  const optionItems = items.map((item) => {
+    return (
+      <ItemComponent
+        key={item.name}
+        name={item.name}
+        imagePath={item.imagePath}
+      />
+    );
+  });
+
+  return <Row>{optionItems}</Row>;
 }
